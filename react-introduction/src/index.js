130e678b735af2153d3891bf3fa450022736a533
Connor <connorturlan@gmail.com>
2022-07-14 11:23:23 +0930

    finished basic game

--- react-introduction/src/index.js
+++ react-introduction/src/index.js
@@ -2,31 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-class ShoppingList extends React.Component {
-	render() {
-		return (
-			<div className="shopping-list">
-				<h1>Shopping List for {this.props.name}</h1>
-				<ul>
-					<li>Instagram</li>
-					<li>WhatsApp</li>
-					<li>Oculus</li>
-				</ul>
-			</div>
-		);
-	}
-}
-
-class _Square extends React.Component {
-	render() {
-		return (
-			<button className="square" onClick={() => this.props.onClick()}>
-				{this.props.value}
-			</button>
-		);
-	}
-}
-
 function calculateWinner(squares) {
 	const lines = [
 		[0, 1, 2],
@@ -38,6 +13,7 @@ function calculateWinner(squares) {
 		[0, 4, 8],
 		[2, 4, 6],
 	];
+
 	for (let i = 0; i < lines.length; i++) {
 		const [a, b, c] = lines[i];
 		if (
@@ -48,6 +24,7 @@ function calculateWinner(squares) {
 			return squares[a];
 		}
 	}
+
 	return null;
 }
 
@@ -60,47 +37,18 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-			xIsNext: true,
-		};
-	}
-
-	handleClick(i) {
-		const squares = [...this.state.squares];
-
-		// valiate that the square is free.
-		if (calculateWinner(squares || squares[i] !== null)) {
-			return;
-		}
-
-		squares[i] = this.state.xIsNext ? "X" : "O";
-		this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
-	}
-
 	renderSquare(i) {
 		return (
 			<Square
-				value={this.state.squares[i]}
-				onClick={() => this.handleClick(i)}
+				value={this.props.squares[i]}
+				onClick={() => this.props.onClick(i)}
 			/>
 		);
 	}
 
 	render() {
-		const winner = calculateWinner(this.state.squares);
-		let status;
-		if (winner) {
-			status = `Winner: ${winner}`;
-		} else {
-			status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-		}
-
 		return (
 			<div>
-				<div className="status">{status}</div>
 				<div className="board-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
@@ -122,15 +70,83 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			history: [
+				{
+					squares: Array(9).fill(null),
+				},
+			],
+			stepNumber: 0,
+			xIsNext: true,
+		};
+	}
+	handleClick(i) {
+		// get the current game state.
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
+		const current = history[history.length - 1];
+		const squares = current.squares.slice();
+
+		// valiate that the square is free.
+		if (calculateWinner(squares) || squares[i]) {
+			return;
+		}
+
+		// update the next player's state.
+		squares[i] = this.state.xIsNext ? "X" : "O";
+
+		// update the game state.
+		this.setState({
+			history: history.concat({
+				squares: squares,
+			}),
+			stepNumber: history.length,
+			xIsNext: !this.state.xIsNext,
+		});
+	}
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: step % 2 === 0,
+		});
+	}
 	render() {
+		// get the current game state.
+		const history = this.state.history;
+		const current = history.at(this.state.stepNumber);
+		const winner = calculateWinner(current.squares);
+
+		// update past moves.
+		const moves = history.map((step, move) => {
+			const desc = move ? `Go to move #${move}` : "Go to game start";
+			return (
+				<li key={move}>
+					<button onClick={() => this.jumpTo(move)}>{desc}</button>
+				</li>
+			);
+		});
+
+		// update the status.
+		let status;
+		if (winner) {
+			status = `Winner: ${winner}`;
+		} else {
+			status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+		}
+
+		// render the game board.
 		return (
 			<div className="game">
 				<div className="game-board">
-					<Board />
+					<Board
+						squares={current.squares}
+						onClick={(i) => this.handleClick(i)}
+					/>
 				</div>
 				<div className="game-info">
-					<div>{/* status */}</div>
-					<ol>{/* TODO */}</ol>
+					<div>{status}</div>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 		);
